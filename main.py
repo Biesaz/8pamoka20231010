29cc0ebf53cc671ec9ebec24701f43714a58e148
Vygantas Vilius <biesaz@gmail.com>
2023-10-10 23:24:27 +0300

    add calculator.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Bet kas")
+# kuriame sakas
+
+# biesaz@R5-3600X MINGW64 /f/VygaPytons/CodeAcademy/8pamoka20231010 (main)
+# $ git checkout -b staging
+# Switched to a new branch 'staging'
+# biesaz@R5-3600X MINGW64 /f/VygaPytons/CodeAcademy/8pamoka20231010 (staging)
