1076e513f5a1a6ea4895ea73470f823d10eb76d1
Vygantas Vilius <biesaz@gmail.com>
2023-10-11 00:21:11 +0300

    add calculate-letters.py

---
+++ calculate-letters.py
@@ -0,0 +1,30 @@
+# [20:21] Mindaugas Rudzevičius
+
+# write a program witch takes at least 5 minimum words (use only 1 input) separated by comma. 
+# From those words , make a dictionary where the key is a letter and value is a number of the 
+# frequency the letter did appear in all those words. Letters should be in alphabetical order.
+
+# Everything should be done thorugh github workflow.
+
+# isprinitinti dictionary pvz alibaba a-3. reikia naujos sakos
+
+# minimum_five_words = input("Enter at least 5 minimum words separated by comma: ")
+# make_dictionary = {}
+# print(minimum_five_words)
+
+# splitinti padaryti i lista kableliu split builin
+# key: value
+# letter: a number of the  frequency the letter did appear in all those words
+
+# name = input ("Enter your name: ")
+# surname = input("Enter your surname: ")
+# age = int(input("Enter your age: "))
+# #1 variantas
+# my_dictionary = {"name": name, "surname": surname, "age": age}
+# print(my_dictionary)
+# # 2 variantas
+# my_dictionary = {}
+# my_dictionary["name"] = name
+# my_dictionary["surname"] = surname
+# my_dictionary["age"] = age
+# print(my_dictionary) 
