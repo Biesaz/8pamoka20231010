989b9f4d9dadfd37ab42673c7ea7287b2724d433
Vygantas Vilius <biesaz@gmail.com>
2023-10-12 00:07:00 +0300

    abbz

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# kuriame sakas
+# kuriame sakas(branch)
 
 # biesaz@R5-3600X MINGW64 /f/VygaPytons/CodeAcademy/8pamoka20231010 (main)
 # $ git checkout -b staging
 # Switched to a new branch 'staging'
 # biesaz@R5-3600X MINGW64 /f/VygaPytons/CodeAcademy/8pamoka20231010 (staging)
+
+# biesaz@R5-3600X MINGW64 /f/VygaPytons/CodeAcademy/8pamoka20231010 (create-calculator)
+# $ git checkout staging 
+# Switched to branch 'staging'
+
